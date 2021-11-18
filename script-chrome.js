fcd8ac088c354bbc2ba17f834f340b9792091746
filanico <filanico@gmail.com>
2021-11-18 18:25:04 +0100

    Primo commit

---
+++ script-chrome.js
@@ -0,0 +1,62 @@
+//////////// CONFIGURAZIONE ///////////////////////////////////////////////////
+// const adesso = new Date();
+// const adessoAsString = `${adesso.getFullYear()}-${adesso.getMonth()+1}-${adesso.getDate()}`;
+const checkEveryMsecs=4000;
+const rate = 0;
+const payload  = {
+  asc: "desc",
+  league: "all",
+  playing_status: "all",
+  position: "all",
+  scarcity: "all",
+  order: "endingAt",
+  bmp: true,
+  gw: 0,
+  limit: 20,
+  max_price: -1,
+  min_price: -1,
+  offset: 0,
+  teams: [],
+  u23: false,
+  watchlist: false
+};
+////////////////////////////////////////////////////////////////////////////////
+
+const delay = async(msecs)=>new Promise((res,rej)=>{setTimeout(()=>{res();},msecs)});
+
+
+(async()=>{
+	// return;
+	while(true){
+		var resp = await fetch("https://www.soraredata.com/api/offers/ongoingOffers", {
+			"credentials": "omit",
+			"headers": {
+				"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0",
+				"Accept": "application/json, text/plain, /",
+				"Accept-Language": "en-US,en;q=0.5",
+				"Content-Type": "application/json",
+				"Alt-Used": "www.soraredata.com",
+				"Pragma": "no-cache",
+				"Cache-Control": "no-cache",
+				"Sec-Fetch-Dest": "empty",
+				"Sec-Fetch-Mode": "cors",
+				"Sec-Fetch-Site": "same-origin"
+			},
+			"Referer": "https://www.soraredata.com/publicOffers",
+			"body": JSON.stringify(payload),
+			"method": "POST",
+			"mode": "cors"
+			}
+		); 
+		// console.log("FATTO");
+		resp = await resp.json();
+		var data = resp
+			.offers
+			.filter( o => (o.bmp-o.offer.ReceiveAmountInWei)/o.offer.ReceiveAmountInWei <= rate )
+			.map( o => o.player.DisplayName)
+		;
+		console.log( data.join("\n") );
+		// console.log( resp.offers );
+		await delay(checkEveryMsecs);
+	}
+})();
