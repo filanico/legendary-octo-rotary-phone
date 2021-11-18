ef1c59fe8a1298152a0501e3df8de0cb031ca2e4
filanico <filanico@gmail.com>
2021-11-18 18:39:14 +0100

    aggiornamento

--- script-chrome.js
+++ script-chrome.js
@@ -2,7 +2,7 @@
 // const adesso = new Date();
 // const adessoAsString = `${adesso.getFullYear()}-${adesso.getMonth()+1}-${adesso.getDate()}`;
 const checkEveryMsecs=4000;
-const rate = 0;
+const rate = 0.5;
 const payload  = {
   asc: "desc",
   league: "all",
@@ -28,6 +28,8 @@ const delay = async(msecs)=>new Promise((res,rej)=>{setTimeout(()=>{res();},msec
 (async()=>{
 	// return;
 	while(true){
+		var adesso = new Date();
+		var adessoAsString = `[${adesso.getFullYear()}-${adesso.getMonth()+1}-${adesso.getDate()} ${adesso.getHours()}:${adesso.getMinutes().toString().padStart(2,0)}:${adesso.getSeconds().toString().padStart(2,0)}]`;
 		var resp = await fetch("https://www.soraredata.com/api/offers/ongoingOffers", {
 			"credentials": "omit",
 			"headers": {
@@ -53,8 +55,14 @@ const delay = async(msecs)=>new Promise((res,rej)=>{setTimeout(()=>{res();},msec
 		var data = resp
 			.offers
 			.filter( o => (o.bmp-o.offer.ReceiveAmountInWei)/o.offer.ReceiveAmountInWei <= rate )
-			.map( o => o.player.DisplayName)
+			.map( o => {
+				var _rate = (o.bmp-o.offer.ReceiveAmountInWei)/o.offer.ReceiveAmountInWei;
+				var _extraInfo = o.bmp == o.offer.ReceiveAmountInWei ? '' : `(bmp:${o.bmp} ReceiveAmountInWei${o.offer.ReceiveAmountInWei})`;
+				return `${adessoAsString} ${o.player.DisplayName} ${_extraInfo}`;
+			})
 		;
+		if( data.length == 0 )
+			data = ['Niente :('];
 		console.log( data.join("\n") );
 		// console.log( resp.offers );
 		await delay(checkEveryMsecs);
